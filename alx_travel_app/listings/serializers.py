ee45a99551fc91d2f01b29e81081efc8c70eb116
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-05-28 18:44:49 +0000

    fdq

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,21 +1,13 @@
+# listings/serializers.py
 from rest_framework import serializers
 from .models import Listing, Booking
-
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
 
-
 class BookingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Booking
-        fields = '__all__'
-
-class BookingSerializer(serializers.ModelSerializer):
-    listing = ListingSerializer(read_only=True)
-
     class Meta:
         model = Booking
         fields = '__all__'
